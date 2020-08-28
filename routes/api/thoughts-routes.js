6697e04df38ceebd54876a124921ff2b63c3ce99
John Nielsen <johnhayesnielsen@gmail.com>
2020-08-28 14:39:24 -0600

    updated routes/controllers for more thoughts functionality

--- routes/api/thoughts-routes.js
+++ routes/api/thoughts-routes.js
@@ -1,17 +1,26 @@
 const router = require('express').Router();
-const { addThoughts, removeThoughts, addReaction, removeReaction } = require('../../controllers/thoughts-controller');
+const { 
+    addThoughts, 
+    removeThoughts, 
+    addReaction, 
+    removeReaction 
+
+} = require('../../controllers/thoughts-controller');
 
 // /api/thoughts/<userId>
-router.route('/:userId')
+router
+.route('/:userId')
 .post(addThoughts);
 
 // /api/thoughts/<userId>/<thoughtsId>
-router.route('/:userId/:thoughtsId')
+router
+.route('/:userId/:thoughtsId')
 .put(addReaction)
 .delete(removeThoughts);
 
 // /api/thoughts/<userId>/<thoughtsId>/<reactionId>
-router.route('/:userId/:thoughtsId/:reactionId')
+router
+.route('/:userId/:thoughtsId/:reactionId')
 .delete(removeReaction);
 
 
