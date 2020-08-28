4f1d4554cc8c141033af8f44bdc939fba260080b
John Nielsen <johnhayesnielsen@gmail.com>
2020-08-28 10:00:21 -0600

    added Thoughts and Reactions Schema

---
+++ models/Thoughts.js
@@ -0,0 +1,68 @@
+const { Schema, model, Types } = require('mongoose');
+const moment = require('moment');
+
+const ReactionSchema = new Schema({
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId()
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxLength: 280
+    },
+    writtenBy: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+    }
+},
+    {
+        toJSON: {
+            getters: true
+        }
+    }
+);
+
+const ThoughtsSchema = new Schema({
+    thoughtText: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 1,
+        maxlength: 280
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+    },
+    writtenBy: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    reactions: [ReactionSchema]
+},
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+//get total count of comments and replies on retrieval
+ThoughtsSchema.virtual('reactionCount').get(function () {
+    return this.replies.length;
+});
+
+const Thoughts = model('Thoughts', ThoughtsSchema);
+
+module.exports = Thoughts
+
