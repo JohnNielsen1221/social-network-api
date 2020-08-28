90ae81a32c80f15bc657d782e5b0452342d80d91
John Nielsen <johnhayesnielsen@gmail.com>
2020-08-27 19:36:36 -0600

    created UserSchema

---
+++ models/User.js
@@ -0,0 +1,46 @@
+const { Schema, model } = require('mongoose');
+const moment = require('moment');
+
+const UserSchema = new Schema(
+    {
+        userName: {
+            type: String,
+            unique: true,
+            required: 'Username is required',
+            trim: true
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: 'Email address is required',
+            match: [/.+@.+\..+/]
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thoughts'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true
+        },
+        id: false
+    }
+);
+
+UserSchema.virtual('friendCount').get(function () {
+    return this.replies.length;
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
+
